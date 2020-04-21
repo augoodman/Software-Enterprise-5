5e3262d7447eafa8b6eca02b8b044f6e1a3076e3
augoodma <augoodma@asu.edu>
2020-04-20 20:29:16 -0400

    Submission 1 for Assignment 5

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+package main.java;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world.");
+    }
+    public static int getTestResult() {
+        return 0;
+    }
+}
---
+++ src/test/Assign5Test.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import main.java.Main;
+public class Assign5Test {
+
+    @Test
+    public void simpleTest() {
+        assertEquals(Main.getTestResult(), 0);
+    }
+
+}
